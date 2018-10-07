189a218465a58e192a804a74080cf937995ed188
Greg <rlneumiller@gmail.com>
2018-10-07 09:16:43 -0700

    intitial import

---
+++ UIComponents/Assets/Scripts/DieForMe.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DieForMe : MonoBehaviour
+{
+
+	public void OnDestroy()
+	{
+		Debug.Log("AHHHHhhhh!!!!!!");
+	}
+
+}
+
